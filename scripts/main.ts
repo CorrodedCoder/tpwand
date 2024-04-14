bb18dedc92df8c2bd762fad381e7333dcbfa2457
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2024-04-14 15:58:04 +0100

    Extract event checks into functions

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,4 +1,4 @@
-import { world, system, Player, Vector3, ItemUseOnBeforeEvent, ItemUseBeforeEvent, ItemUseAfterEvent } from "@minecraft/server";
+import { world, system, Player, Vector3, ItemUseOnBeforeEvent, ItemUseOnAfterEvent, ItemUseBeforeEvent, ItemUseAfterEvent } from "@minecraft/server";
 import { ModalFormData, ActionFormData, ActionFormResponse, MessageFormData } from "@minecraft/server-ui";
 
 class wellKnownLocations {
@@ -101,106 +101,122 @@ function wellKnownLocationUI(player: Player) {
   });
 }
 
+function isTpWandAdminEvent(event: ItemUseAfterEvent|ItemUseOnAfterEvent): boolean {
+  if(
+    (event.itemStack.typeId === "minecraft:command_block")
+    &&
+    (event.itemStack.nameTag === "tpwandadmin")
+  ){
+    return true;
+  }
+  return false;
+}
+
+function isTpWandEvent(event: ItemUseAfterEvent): boolean {
+  if(
+    (event.itemStack.typeId === "minecraft:stick")
+    &&
+    (event.itemStack.nameTag === "tpwand")
+  ){
+    return true;
+  }
+  return false;
+}
+
 world.beforeEvents.itemUse.subscribe((event: ItemUseBeforeEvent) => {
-  if (event.itemStack.typeId === "minecraft:command_block"){
-    if(event.itemStack.nameTag === "tpwandadmin" ) {
-      event.cancel = true;
-      system.run(() => wellKnownLocationUI(event.source));
-    }
+  if(isTpWandAdminEvent(event)) {
+    event.cancel = true;
+    system.run(() => wellKnownLocationUI(event.source));
   }
 });
 
 world.beforeEvents.itemUseOn.subscribe((event: ItemUseOnBeforeEvent) => {
-  if (event.itemStack.typeId === "minecraft:command_block"){
-    if(event.itemStack.nameTag === "tpwandadmin" ) {
+  if (isTpWandAdminEvent(event)){
       event.cancel = true;
-    }
   }
 });
 
 world.afterEvents.itemUse.subscribe((event: ItemUseAfterEvent) => {
-  if (event.itemStack.typeId === "minecraft:stick"){
-    if(event.itemStack.nameTag === "tpwand" ) {
-      const player: Player = event.source;
-      const initForm = new ActionFormData()
-        .title('tpwand')
-        .body('Choose the player you would like to teleport to')
-        .button('Teleport to world spawn point')
-        .button('Teleport to another players location')
-        .button('Teleport to well known location');
-      initForm.show(player).then((response: ActionFormResponse) => {
-        switch(response.selection){
-          case undefined:{
-            break;
-          }
-          case 0:{
-            const loc = world.getDefaultSpawnLocation();
-            // If Y is this value then there is no default world spawn location set.
-            if( loc.y !== 32767){
-              player.sendMessage("Your wish is my command...");
-              player.teleport(world.getDefaultSpawnLocation());
-            }
-            break;
-          }
-          case 1:{
-            const other_players: Player[] = world.getAllPlayers();
-            if(other_players.length === 1){
-              new ActionFormData().title("tpwand").body("No other players available!").button("Okay").show(player);
-              return;
-            }
-            let form = new ModalFormData()
-              .title("tpwand");
-            let player_names: string[] = [];
-            for(const other of other_players){
-              if( other.name !== player.name ) {
-                player_names.push(other.name);
-              }
-            }
-            form.dropdown('Teleport to player', player_names);
-            form.show(event.source).then(r => {
-              if(r.canceled) {
-                return;
-              }
-              if(r.formValues){
-                const index = Number(r.formValues[0]);
-                const other = other_players.find((op: Player) => {
-                  return op.name === player_names[index];
-                });
-                if(other){
-                  player.sendMessage("Your wish is my command...");
-                  player.teleport(other.location);
-                }
-              }
-            }).catch((e) => {
-              console.error(e, e.stack);
-            });
-            break;
-          }
-          case 2:{
-            const locations = new wellKnownLocations(world.getDynamicProperty('tpwand_locations') as string);
-            if(locations.count() === 0){
-              new ActionFormData().title("tpwand").body("No locations available!").button("Okay").show(player);
-              return;
-            }
-            let form = new ModalFormData()
-              .title("tpwand")
-              .dropdown('Teleport to location', locations.names());
-            form.show(event.source).then(r => {
-              if(r.canceled) {
-                return;
-              }
-              if(r.formValues){
-                player.sendMessage("Your wish is my command...");
-                player.teleport(locations.get(r.formValues[0] as number));
-              }
-            }).catch((e) => {
-              console.error(e, e.stack);
-            });
-            break;
-          }
+  if (isTpWandEvent(event)){
+    const player: Player = event.source;
+    const initForm = new ActionFormData()
+      .title('tpwand')
+      .body('Choose the player you would like to teleport to')
+      .button('Teleport to world spawn point')
+      .button('Teleport to another players location')
+      .button('Teleport to well known location');
+    initForm.show(player).then((response: ActionFormResponse) => {
+      switch(response.selection){
+        case undefined:{
+          break;
         }
-      });        
-    }
+        case 0:{
+          const loc = world.getDefaultSpawnLocation();
+          // If Y is this value then there is no default world spawn location set.
+          if( loc.y !== 32767){
+            player.sendMessage("Your wish is my command...");
+            player.teleport(world.getDefaultSpawnLocation());
+          }
+          break;
+        }
+        case 1:{
+          const other_players: Player[] = world.getAllPlayers();
+          if(other_players.length === 1){
+            new ActionFormData().title("tpwand").body("No other players available!").button("Okay").show(player);
+            return;
+          }
+          let form = new ModalFormData()
+            .title("tpwand");
+          let player_names: string[] = [];
+          for(const other of other_players){
+            if( other.name !== player.name ) {
+              player_names.push(other.name);
+            }
+          }
+          form.dropdown('Teleport to player', player_names);
+          form.show(event.source).then(r => {
+            if(r.canceled) {
+              return;
+            }
+            if(r.formValues){
+              const index = Number(r.formValues[0]);
+              const other = other_players.find((op: Player) => {
+                return op.name === player_names[index];
+              });
+              if(other){
+                player.sendMessage("Your wish is my command...");
+                player.teleport(other.location);
+              }
+            }
+          }).catch((e) => {
+            console.error(e, e.stack);
+          });
+          break;
+        }
+        case 2:{
+          const locations = new wellKnownLocations(world.getDynamicProperty('tpwand_locations') as string);
+          if(locations.count() === 0){
+            new ActionFormData().title("tpwand").body("No locations available!").button("Okay").show(player);
+            return;
+          }
+          let form = new ModalFormData()
+            .title("tpwand")
+            .dropdown('Teleport to location', locations.names());
+          form.show(event.source).then(r => {
+            if(r.canceled) {
+              return;
+            }
+            if(r.formValues){
+              player.sendMessage("Your wish is my command...");
+              player.teleport(locations.get(r.formValues[0] as number));
+            }
+          }).catch((e) => {
+            console.error(e, e.stack);
+          });
+          break;
+        }
+      }
+    });
   }
 });
 
